d36f75405f6d93ec4c3708d8315e6ad51bc8a8c6
Odrec <odrec@Odrecs-MacBook-Pro.local>
2026-02-04 12:36:27 +0100

    feat(recommendations): Add recommendations component exports

---
+++ src/components/recommendations/index.ts
@@ -0,0 +1,6 @@
+// Export all recommendation components
+
+export { RecommendationCard } from './RecommendationCard'
+export { RecommendationList } from './RecommendationList'
+export { ScheduleAnalysis } from './ScheduleAnalysis'
+export { BatchAddButton } from './BatchAddButton'
