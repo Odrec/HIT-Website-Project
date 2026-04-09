ee22aced6afb63b6611e7c830be484ab8157e26c
Odrec <odrec@Odrecs-MacBook-Pro.local>
2026-04-09 06:29:12 -0600

    feat: add ClusterIcon component

---
+++ src/components/ui/cluster-icon.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+
+interface ClusterIconProps {
+  icon: string | null | undefined
+  name: string
+  size?: number
+  className?: string
+}
+
+export function ClusterIcon({ icon, name, size = 24, className }: ClusterIconProps) {
+  if (!icon) return null
+
+  return (
+    <Image
+      src={`/clusters/${icon}`}
+      alt={name}
+      width={size}
+      height={size}
+      className={className}
+    />
+  )
+}
